518527f8188770c700fb79fe1f4eb0b9a17227aa
Alice <auvalice@outlook.com>
2021-12-02 21:06:09 +0100

    aoc 1 + 2

---
+++ day1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+var lines = File.ReadAllLines("input.txt");
+
+int count = 0;
+
+for (int index = 3; index < lines.Length; index++)
+{
+    /*
+        When checking the sum of the first three numbers and the last three numbers,
+        the second and third numbers do not vary. We only need to check if the fourth
+        number is larger or not than the first.
+     */
+
+    int a = int.Parse(lines[index - 3]);
+    // int b = int.Parse(lines[index - 2]);
+    // int c = int.Parse(lines[index - 1]);
+    int d = int.Parse(lines[index]);
+
+    if(d > a) {
+        count++;
+    }    
+
+}
+
+Console.WriteLine(count);
---
+++ day2/Program.cs
@@ -0,0 +1,28 @@
+﻿var lines = File.ReadAllLines("input.txt");
+
+(int x, int y) coords = (0, 0);
+int aim = 0;
+
+foreach (var line in lines)
+{
+    var values = line.Split(' ');
+
+    var direction = values[0];
+    var amount = int.Parse(values[1]);
+
+    switch (direction)
+    {
+        case "forward":
+            coords.x += amount;
+            coords.y += amount * aim;
+            break;
+        case "down":
+            aim += amount;
+            break;
+        case "up":
+            aim -= amount;
+            break;
+    }
+}
+
+Console.WriteLine(coords.x * coords.y);
